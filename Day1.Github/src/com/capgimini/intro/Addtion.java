c059656ad4b1a468e00e1b7d82fb4c760d5f0067
sambhaji <sambhaji@LAPTOP-CSDG9POB>
2022-07-11 17:49:56 +0530

    secondprogram

---
+++ Day1.Github/src/com/capgimini/intro/Addtion.java
@@ -0,0 +1,14 @@
+package com.capgimini.intro;
+
+public class Addtion {
+
+	public static void main(String[] args) {
+		int a=12;
+		int b=20;
+		int c=a+b;
+		
+		System.out.println("Addtion is:"+c);
+
+	}
+
+}
