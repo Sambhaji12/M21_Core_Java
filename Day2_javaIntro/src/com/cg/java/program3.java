c3c40a85a494a2f52093ca0843ec6fed40909344
sambhaji <sambhaji@LAPTOP-CSDG9POB>
2022-07-12 15:48:02 +0530

    fourth commit

---
+++ Day2_javaIntro/src/com/cg/java/program3.java
@@ -0,0 +1,16 @@
+package com.cg.java;
+import java.util.*;
+
+public class program3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		double d1=sc.nextDouble();
+		
+		
+		System.out.println("double value of d1"+d1);
+
+	}
+
+}
