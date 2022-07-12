8ff54d4ed9bf4f0da2fb96e5bbb218136660adee
sambhaji <sambhaji@LAPTOP-CSDG9POB>
2022-07-12 13:27:30 +0530

    Signed-off-by: sambhaji <sambhaji@LAPTOP-CSDG9POB>

---
+++ Day2_javaIntro/src/com/cg/java/program1.java
@@ -0,0 +1,16 @@
+package com.cg.java;
+import java.util.*; 
+public class program1 {
+
+	public static void main(String[] args) {
+		
+		Scanner Sc=new Scanner(System.in);
+		int a=Sc.nextInt();
+		
+		System.out.println("values of a: "+a);
+		
+		Sc.close();
+
+	}
+
+}
---
+++ Day2_javaIntro/src/com/cg/java/program2.java
@@ -0,0 +1,13 @@
+package com.cg.java;
+import java.util.*;
+public class program2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		String str= sc.nextLine();
+		System.out.println(str);
+
+	}
+
+}
