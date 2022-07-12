4d7859eda51d7156f8550cfa06ec0ae8ed0316f6
sambhaji <sambhaji@LAPTOP-CSDG9POB>
2022-07-12 16:57:36 +0530

    five commit

---
+++ Day2_javaIntro/src/com/cg/java/program4.java
@@ -0,0 +1,14 @@
+package com.cg.java;
+import java.util.*;
+
+public class program4 {
+
+	public static void main(String[] args) {
+		Scanner s1=new Scanner(System.in);
+		
+		float f1=s1.nextFloat();
+		
+		System.out.println("float is :"+f1);
+	};
+
+}
