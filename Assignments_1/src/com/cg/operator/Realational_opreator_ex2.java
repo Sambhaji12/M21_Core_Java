2755b85f960cd8c1cb082eeb57d515b7ff2df4a5
sambhaji <sambhaji@LAPTOP-CSDG9POB>
2022-07-13 15:49:10 +0530

    Relational_op

---
+++ Assignments_1/src/com/cg/operator/Realational_opreator_ex2.java
@@ -0,0 +1,19 @@
+package com.cg.operator;
+
+import java.util.Scanner;
+
+public class Realational_opreator_ex2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		
+		int b=sc.nextInt();
+		
+		boolean  res=a<b;
+		
+		System.out.println(res);
+
+	}
+
+}
