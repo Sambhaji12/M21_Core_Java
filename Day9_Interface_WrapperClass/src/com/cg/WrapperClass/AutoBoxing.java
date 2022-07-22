b1c6d03b27fe65ba565f4087e572990c640034db
sambhaji <sambhaji@LAPTOP-CSDG9POB>
2022-07-22 10:31:14 +0530

    First commit

---
+++ Day9_Interface_WrapperClass/src/com/cg/WrapperClass/AutoBoxing.java
@@ -0,0 +1,13 @@
+package com.cg.WrapperClass;
+//Convering primitive types to wrapperclass objects::: 
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		int a=20;
+		Integer   i=a;
+		Integer j=new Integer(10);
+		System.out.println(a+" "+i+" "+j);
+
+	}
+
+}
---
+++ Day9_Interface_WrapperClass/src/com/cg/WrapperClass/Unboxing.java
@@ -0,0 +1,11 @@
+package com.cg.WrapperClass;
+//To convert wrapper class object to primitive data types::
+public class Unboxing {
+
+	public static void main(String[] args) {
+		Character c='c';
+		char c1=c;
+		System.out.println(c+" "+c1);
+	}
+
+}
