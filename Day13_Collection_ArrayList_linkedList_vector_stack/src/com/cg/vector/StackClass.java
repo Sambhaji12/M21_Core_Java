efb31fba68d24aeefee72644b296e306d5afbf3d
sambhaji <sambhaji@LAPTOP-CSDG9POB>
2022-07-27 11:20:02 +0530

    First commit

---
+++ Day13_Collection_ArrayList_linkedList_vector_stack/src/com/cg/vector/StackClass.java
@@ -0,0 +1,22 @@
+package com.cg.vector;
+
+import java.util.Stack;
+
+public class StackClass {
+
+	public static void main(String[] args) {
+		Stack <Object> s=new Stack<>();
+		s.push(11);
+		s.push(23);
+		s.push("Sam");
+		
+		System.out.println(s);
+		s.pop();
+		System.out.println(s);
+		
+		
+		
+
+	}
+
+}
