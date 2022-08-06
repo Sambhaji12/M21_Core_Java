7be66932ed24c0363db043ce9c31da7d5d0c4ff6
sambhaji <sambhaji@LAPTOP-CSDG9POB>
2022-08-06 10:46:24 +0530

    first commit

---
+++ Day24_Enum_Regular_Expression/src/com/cg/enumpro/Example2.java
@@ -0,0 +1,19 @@
+package com.cg.enumpro;
+
+public class Example2 
+{
+	enum color
+	{
+		red,green,blue;
+	}
+	public static void main(String[] args) {
+		//color c=color.blue;
+		//System.out.println(c);
+		
+		for(color c:color.values())
+		{
+			System.out.println(c);
+		}
+	}
+
+}
